bfb88bc4a8314d60d03cb3512410698cef6d3db1
Brian Tobal <brian.tobal@gmail.com>
2019-04-21 23:09:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,14 @@
 class Student < User 
+  
   def initialize 
     @knowledge = []
   end     #def 
   
+  def learn(string)
+    @knowledge << string
+  end     #def 
 
+  def knowledge
+    @knowledge
+  end     #def 
 end
